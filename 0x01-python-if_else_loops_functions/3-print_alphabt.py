9fe742d96c0767d278c7ca13ab2d8cf3553b80c5
Gadvin <gadogechi@gmail.com>
2023-10-31 06:37:24 -0700

    update 1.4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for character in range(97, 123):
+    if (character == 101) | (character == 113):
+        continue
     print(chr(character).format(), end="")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 0:
+            first_digit = 0
+        else:
+            first_digit = i
+        print(f"{first_digit}{j}, ", end='')
+print()
