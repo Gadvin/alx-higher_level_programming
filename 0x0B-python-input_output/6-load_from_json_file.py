f60f7a7542869f630b2122f0f28405928f2826b0
Gadvin <gadogechi@gmail.com>
2024-01-02 23:08:34 -0800

    update 2.6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,10 +3,10 @@
 import json
 
 
-def load_from_json_string(my_str):
+def load_from_json_file(my_str):
     """function to 
     desializes a JSON string back to a python object
         -> handles NO exceptions
     """
-     with open(filename) as f:
+     with open(my_str) as f:
         return json.load(f)
