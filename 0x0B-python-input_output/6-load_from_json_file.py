20da7c86ca66ae15c7adef9d84e3bd96ae164b76
Gadvin <gadogechi@gmail.com>
2024-01-02 23:11:01 -0800

    update 2.7

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,9 +4,6 @@ import json
 
 
 def load_from_json_file(my_str):
-    """function to 
-    desializes a JSON string back to a python object
-        -> handles NO exceptions
-    """
+    """function to Create a Python object from a JSON file"""
      with open(my_str) as f:
         return json.load(f)
