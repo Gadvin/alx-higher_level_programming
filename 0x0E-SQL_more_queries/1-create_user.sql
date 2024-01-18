a33f2c31d926baff97b63fbc51f6cf66ff754758
Gadvin <gadogechi@gmail.com>
2024-01-18 11:30:03 -0800

    Update 1.1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that lists privelages of users on server
+-- DML QUERY to do list privelages of users on serve
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
